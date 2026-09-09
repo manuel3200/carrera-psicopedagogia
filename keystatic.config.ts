aa37e598c4328cfab954e9489e1f74a1dace32bd
manuel3200 <manuelortiz1188@gmail.com>
2026-09-09 01:22:19 -0300

    fix(keystatic): set cloud project to psi-evo-1/carrera-psp

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -11,7 +11,7 @@ export default config({
         kind: 'local',
       },
   cloud: {
-    project: 'psi-evo-1/carrera-psicopedagogia',
+    project: 'psi-evo-1/carrera-psp',
   },
   ui: {
     brand: {
